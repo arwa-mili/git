6ae43f71acbb60ba3c68c71f4a520b254013ee21
rw@@arwa <64184160+arwa-mili@users.noreply.github.com>
2022-12-07 00:15:55 +0100

    Add files via upload

---
+++ appvoice.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+import customtkinter as ctk
+import soundfile as sf
+import sounddevice as sd
+
+import whisper
+
+# Select from the following models: "tiny", "base", "small", "medium", "large"
+model = whisper.load_model("small")
+
+
+app = tk.Tk()
+app.geometry("532x632")
+app.title("Voice to text")
+ctk.set_appearance_mode("dark")
+
+main_label = ctk.CTkLabel(
+    height=512, width=512, text_color="black", text_font=("Roboto Medium", -16)
+)
+main_label.place(x=10, y=110)
+
+
+def voice_rec():
+    fs = 48000
+
+    # seconds
+    duration = 5
+    main_label.configure(text="Recording...")
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+    sd.wait()
+
+    # Save as FLAC file at correct sampling rate
+    sf.write("my_Audio_file.flac", myrecording, fs)
+    main_label.configure(text="Recording done")
+
+
+def transcribe():
+    audio = "my_Audio_file.flac"
+
+    # You can provide the language to the model to translate to
+    options = {"fp16": False, "language": None, "task": "transcribe"}
+    results = model.transcribe(audio, **options)
+
+    print(results["text"])
+    main_label.configure(text=results["text"])
+
+
+def translate():
+    audio = "my_Audio_file.flac"
+
+
+    options = {"fp16": False, "language": None, "task": "translate"}
+    results = model.transcribe(audio, **options)
+
+    print(results["text"])
+    main_label.configure(text=results["text"])
+
+
+recordButton = ctk.CTkButton(
+    height=40,
+    width=120,
+    text_font=("Roboto Medium", 20),
+    text_color="white",
+    fg_color=("white", "gray38"),
+    command=voice_rec,
+)
+recordButton.configure(text="Record")
+recordButton.place(x=206, y=60)
+
+transcribeButton = ctk.CTkButton(
+    height=40,
+    width=120,
+    text_font=("Roboto Medium", 20),
+    text_color="white",
+    fg_color=("white", "gray38"),
+    command=transcribe,
+)
+transcribeButton.configure(text="Transcribe")
+transcribeButton.place(x=106, y=150)
+
+translateButton = ctk.CTkButton(
+    height=40,
+    width=120,
+    text_font=("Roboto Medium", 20),
+    text_color="white",
+    fg_color=("white", "gray38"),
+    command=translate,
+)
+translateButton.configure(text="Translate")
+translateButton.place(x=306, y=150)
